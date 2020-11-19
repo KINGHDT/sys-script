d0dbebeff53f66e32642241e290961df72f979f5
me <me@sina.com>
2020-11-19 10:44:41 +0800

    调整策略

--- Linux/ssh-port.sh
+++ Linux/ssh-port.sh
@@ -3,17 +3,16 @@
 
 PORT=7013
 if [ ${1} ] ;then
-    if [ "$1" -gt 1024 -a "$1" -lt 65535 ] 2>/dev/null; then
 	PORT=$1
-    fi
 fi
 
 if ! grep -n "^Port" /etc/ssh/sshd_config > /dev/null;then
     sed -i "s/^#Port.*$/Port ${PORT}/" /etc/ssh/sshd_config
+    firewall-cmd --zone=public --add-port=${PORT}/tcp --permanent
+    firewall-cmd --reload
+    systemctl restart sshd
 fi
-firewall-cmd --zone=public --add-port=${PORT}/tcp --permanent
-firewall-cmd --reload
-systemctl restart sshd
 
 echo "=========================================="
+# 颜色显示
 echo -e "\033[36mSSHd [ `grep -n "^Port.*" /etc/ssh/sshd_config | cut -d ":" -f 2` ]\033[0m"
