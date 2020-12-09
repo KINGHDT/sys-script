19d355651d79cb1c3faf1c72c4c49dda764c5932
me <me@sina.com>
2020-12-09 19:16:36 +0800

    jdk下载源修改为国内191版

--- Linux/install_jdk.sh
+++ Linux/install_jdk.sh
@@ -6,15 +6,20 @@ fi
 
 yum -y remove *jdk*
 
-echo "Now start download jdk-8u212-linux-x64.tar.gz"
-wget https://github.com/frekele/oracle-java/releases/download/8u212-b10/jdk-8u212-linux-x64.tar.gz
-tar zxvf jdk-8u212-linux-x64.tar.gz -C /usr/local/
 
+echo "Now start download jdk-8u212-linux-x64.tar.gz"
+# wget https://github.com/frekele/oracle-java/releases/download/8u212-b10/jdk-8u212-linux-x64.tar.gz
+# tar zxvf jdk-8u212-linux-x64.tar.gz -C /usr/local/
+wget https://repo.huaweicloud.com/java/jdk/8u191-b12/jdk-8u191-linux-x64.tar.gz
+tar zxvf jdk-8u191-linux-x64.tar.gz -C /usr/local/
+# delete source file
+rm -rf jdk-8u191-linux-x64.tar.gz
+# 添加环境变量
 if  ! ``grep -n "JAVA_HOME=" /etc/profile > /dev/null``; then
 		echo "		Add jdk path to system"
 	    echo '' >> /etc/profile
 	    echo '# java ' >> /etc/profile
-	    echo 'export JAVA_HOME=/usr/local/jdk1.8.0_212' >> /etc/profile
+	    echo 'export JAVA_HOME=/usr/local/jdk1.8.0_191' >> /etc/profile
 	    echo 'export JRE_HOME=${JAVA_HOME}/jre' >> /etc/profile
 	    echo 'export CLASSPATH=.:${JAVA_HOME}/lib:${JRE_HOME}/lib' >> /etc/profile
 	    echo 'export  PATH=${JAVA_HOME}/bin:$PATH' >> /etc/profile
@@ -22,5 +27,4 @@ if  ! ``grep -n "JAVA_HOME=" /etc/profile > /dev/null``; then
 fi
 echo "#############################################"
 java -version
-# delete source file
-rm -rf jdk-8u212-linux-x64.tar.gz
+
